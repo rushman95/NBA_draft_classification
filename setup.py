75c58eac7bee498f568d1695222b980d7d6461b8
Rushab Shah <rush@192-168-68-120.tpgi.com.au>
2023-08-22 12:55:03 +1000

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Building a classifier to predict if college NBA player will be drafted based on statistics',
+    author='Rushab Shah',
+    license='BSD-3',
+)
